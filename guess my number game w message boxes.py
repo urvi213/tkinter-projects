9041b360624f6851649d3f85751d44c7ac0e4336
urvi213 <urvimaheshwari1234@gmail.com>
2024-09-01 12:17:03 +0100

    Update guess my number game w message boxes.py

--- guess my number game w message boxes.py
+++ guess my number game w message boxes.py
@@ -5,6 +5,11 @@ import random
 
 random_num = random.randint(1,20)
 
+def start():
+     messagebox.showinfo("start msg","i'm thinking of a number between 1 and 20. can you guess it ?? >:3")
+     start_frame.pack_forget()
+     game_frame.pack()
+
 def check():
     if num_entry.get().isdigit() == False:
         messagebox.showinfo("invalid input msg","invalid input :(")
@@ -19,12 +24,21 @@ window = Tk()
 window.config(background="white")
 window.title("guess my number :3")
 
-num_entry = Entry(window,font=font.Font(size=15))
+start_frame = Frame(window)
+
+welcome_label = Label(start_frame,text="welcome to my game !! ^_^")
+welcome_label.pack(pady=10)
+start_btn = Button(start_frame,text="start !!",command=start)
+start_btn.pack(pady=10)
+
+start_frame.pack()
+
+game_frame = Frame(window)
+
+num_entry = Entry(game_frame,font=font.Font(size=15))
 num_entry.pack(pady=10)
 
-submit_btn = Button(window,text="submit",font=font.Font(size=15),bg="pink",command=check)
+submit_btn = Button(game_frame,text="submit",font=font.Font(size=15),bg="pink",command=check)
 submit_btn.pack(pady=10)
-
-messagebox.showinfo("start msg","i'm thinking of a number between 1 and 20. can you guess it ?? >:3")
 
 window.mainloop()
