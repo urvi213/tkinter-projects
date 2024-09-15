086f9effb654d5962896cea6e94a99016aee5cb6
urvi213 <urvimaheshwari1234@gmail.com>
2024-09-15 12:43:55 +0100

    Update timer.py

--- timer.py
+++ timer.py
@@ -9,7 +9,7 @@ root.title("timer")
 
 hours = StringVar()
 minutes = StringVar()
-seconds = StringVar()
+seconds = StringVar() # tkinter variable that syncs with the value of an entry box
 
 def reset():
     hours.set("00")
@@ -17,13 +17,19 @@ def reset():
     seconds.set("00")
 
 def start():
+    
+    start_btn.configure(state="disabled")
+    hours_entry.config(state="disabled")
+    minutes_entry.config(state="disabled")
+    seconds_entry.config(state="disabled")
+
     try:
         total_seconds = int(hours.get())*3600+int(minutes.get())*60+int(seconds.get())
     except:
         messagebox.showerror("incorrect value","enter a correct value")
 
     while total_seconds >= 0:
-        m, s = divmod(total_seconds,60) 
+        m, s = divmod(total_seconds,60) # divide total seconds by 60, returns (result,remainder), assign that to variables m s
         h=0
         if m > 60:
             h,m = divmod(m,60)
@@ -32,11 +38,18 @@ def start():
         seconds.set("{:02d}".format(s))
         root.update()
         time.sleep(1)
-        total_seconds -= 1
+        total_seconds -= 1   
+
+    start_btn.configure(state="active")
+    hours_entry.config(state="normal")
+    minutes_entry.config(state="normal")
+    seconds_entry.config(state="normal")
+    reset()
+
 
 reset()
 
-hours_entry = Entry(root,width=30,textvariable=hours)
+hours_entry = Entry(root,width=30,textvariable=hours) # assign text variable to entry box
 hours_entry.place(x=10,y=10)
 
 minutes_entry = Entry(root,width=30,textvariable=minutes)
