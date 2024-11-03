78c024e6d20cf0d249f1df42f9c0640ca22d1fa5
urvi213 <urvimaheshwari1234@gmail.com>
2024-11-03 12:57:26 +0000

    Update address book.py

--- address book.py
+++ address book.py
@@ -1,6 +1,38 @@
 from tkinter import *
 from tkinter.filedialog import asksaveasfile
 from tkinter.filedialog import askopenfile
+from tkinter import messagebox
+
+address_book = {}
+
+def clear_boxes():
+    name_entry.delete(0,END)
+    address_entry.delete(0,END)
+    email_entry.delete(0,END)
+    mobile_entry.delete(0,END)
+    birthday_entry.delete(0,END)
+
+def display(event):
+    details_window = Toplevel(root)
+    name = book_display.get(book_display.curselection())
+    details_text = "NAME: "+name
+    details_list= address_book[name]
+    details_text += "\nADDRESS: "+details_list[0]
+    details_text += "\nEMAIL: "+details_list[1]
+    details_text += "\nMOBILE: "+details_list[2]
+    details_text += "\nBIRTHDAY: "+details_list[3]
+    details_label = Label(details_window,text=details_text)
+    details_label.pack()
+    clear_boxes()
+
+def update_add():
+    name = name_entry.get()
+    if name.strip() == "":
+        messagebox.showerror("no name","enter a name")
+    else:
+        if name not in address_book:
+            book_display.insert(END,name)
+        address_book[name] = [address_entry.get(),email_entry.get(),mobile_entry.get(),birthday_entry.get()]
 
 root = Tk()
 root.title("address book :3")
@@ -10,7 +42,8 @@ title_label.grid(row=0,column=1,padx=5,pady=5)
 open_btn = Button(root,text="Open")
 open_btn.grid(row=0,column=2,padx=5,pady=5)
 
-book_display = Listbox(root)
+book_display = Listbox(root) # bind
+book_display.bind("<<ListboxSelect>>",display)
 book_display.grid(row=1,column=0,rowspan=5,columnspan=2,padx=5,pady=5)
 
 name_label = Label(root,text="Name: ")
@@ -44,7 +77,7 @@ edit_btn.grid(row=6,column=0,padx=5,pady=5)
 del_btn = Button(root,text="Delete")
 del_btn.grid(row=6,column=1,padx=5,pady=5)
 
-updateAdd_btn = Button(root,text="Update/Add")
+updateAdd_btn = Button(root,text="Update/Add",command=update_add)
 updateAdd_btn.grid(row=6,column=2,columnspan=2,padx=5,pady=5)
 
 save_btn = Button(root,text="Save")
