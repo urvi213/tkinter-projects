2b46fe3975bc1b6f9644d3901ef606b03fbed3bf
urvi213 <urvimaheshwari1234@gmail.com>
2024-11-10 12:46:36 +0000

    Update address book.py

--- address book.py
+++ address book.py
@@ -2,8 +2,13 @@ from tkinter import *
 from tkinter.filedialog import asksaveasfile
 from tkinter.filedialog import askopenfile
 from tkinter import messagebox
+import os
 
 address_book = {}
+
+def clear_list():
+    book_display.delete(0,END)
+    address_book.clear()
 
 def clear_boxes():
     name_entry.delete(0,END)
@@ -14,16 +19,20 @@ def clear_boxes():
 
 def display(event):
     details_window = Toplevel(root)
-    name = book_display.get(book_display.curselection())
-    details_text = "NAME: "+name
-    details_list= address_book[name]
-    details_text += "\nADDRESS: "+details_list[0]
-    details_text += "\nEMAIL: "+details_list[1]
-    details_text += "\nMOBILE: "+details_list[2]
-    details_text += "\nBIRTHDAY: "+details_list[3]
-    details_label = Label(details_window,text=details_text)
-    details_label.pack()
-    clear_boxes()
+    index = book_display.curselection()
+    if index:
+        name = book_display.get(index)
+        details_text = "NAME: "+name
+        details_list= address_book[name]
+        details_text += "\nADDRESS: "+details_list[0]
+        details_text += "\nEMAIL: "+details_list[1]
+        details_text += "\nMOBILE: "+details_list[2]
+        details_text += "\nBIRTHDAY: "+details_list[3]
+        details_label = Label(details_window,text=details_text)
+        details_label.pack()
+        clear_boxes()
+    else:
+        messagebox.showerror("no index","select a name")
 
 def update_add():
     name = name_entry.get()
@@ -34,12 +43,56 @@ def update_add():
             book_display.insert(END,name)
         address_book[name] = [address_entry.get(),email_entry.get(),mobile_entry.get(),birthday_entry.get()]
 
+def save():
+    user_file = asksaveasfile(defaultextension="*.txt")
+    if user_file:
+        print(address_book,file=user_file)
+        clear_boxes()
+    else:
+        messagebox.showerror("save failed","file not saved")
+    
+def open():
+    global address_book
+    clear_list()
+    input_file = askopenfile()
+    if input_file:
+        address_book = eval(input_file.read())
+        for key in address_book:
+            book_display.insert(END,key)
+        title_label.config(text=os.path.basename(input_file.name))
+    else:
+        messagebox.showerror("file not selected","file not selected")
+
+def delete():
+    index = book_display.curselection()
+    if index:
+        book_display.delete(index)
+        del address_book[book_display.get(index)]
+        clear_boxes()
+    else:
+        messagebox.showerror("no index","select a name")
+
+def edit():
+    clear_boxes()
+    index = book_display.curselection()
+    if index:
+        name = book_display.get(index)
+        name_entry.insert(0,name)
+        details = address_book.get(name)
+        address_entry.insert(0,details[0])
+        mobile_entry.insert(0,details[1])
+        email_entry.insert(0,details[2])
+        birthday_entry.insert(0,details[3])
+    else:
+        messagebox.showerror("no index","select a name")
+
 root = Tk()
 root.title("address book :3")
 
 title_label = Label(root,text="My Address Book")
 title_label.grid(row=0,column=1,padx=5,pady=5)
-open_btn = Button(root,text="Open")
+
+open_btn = Button(root,text="Open",command=open)
 open_btn.grid(row=0,column=2,padx=5,pady=5)
 
 book_display = Listbox(root) # bind
@@ -71,16 +124,16 @@ birthday_label.grid(row=5,column=2)
 birthday_entry = Entry(root)
 birthday_entry.grid(row=5,column=3,padx=5)
 
-edit_btn = Button(root,text="Edit")
+edit_btn = Button(root,text="Edit",command=edit)
 edit_btn.grid(row=6,column=0,padx=5,pady=5)
 
-del_btn = Button(root,text="Delete")
+del_btn = Button(root,text="Delete",command=delete)
 del_btn.grid(row=6,column=1,padx=5,pady=5)
 
 updateAdd_btn = Button(root,text="Update/Add",command=update_add)
 updateAdd_btn.grid(row=6,column=2,columnspan=2,padx=5,pady=5)
 
-save_btn = Button(root,text="Save")
+save_btn = Button(root,text="Save",command=save)
 save_btn.grid(row=7,column=0,columnspan=4,padx=5,pady=5)
 
 
