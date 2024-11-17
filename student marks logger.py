97dc6833c16eb3baf58081564686be1675fa1269
urvi213 <urvimaheshwari1234@gmail.com>
2024-11-17 12:19:37 +0000

    Update student marks logger.py

--- student marks logger.py
+++ student marks logger.py
@@ -1,5 +1,12 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter.filedialog import askopenfile
+from tkinter.filedialog import asksaveasfile
+import os
+
+def clear_list():
+    display.delete(0,END)
+    marks.clear()
 
 def clear_boxes():
     name_entry.delete(0,END)
@@ -29,6 +36,51 @@ def update_add():
         if name not in marks:
             display.insert(END,name)
         marks[name] = [rollNumber_entry.get(),scienceMarks_entry.get(),mathsMarks_entry.get(),percentage_entry.get()]
+
+def edit():
+    clear_boxes()
+    index = display.curselection()
+    if index:
+        name = display.get(index)
+        name_entry.insert(0,name)
+        details = marks.get(name)
+        rollNumber_entry.insert(0,details[0])
+        scienceMarks_entry.insert(0,details[1])
+        mathsMarks_entry.insert(0,details[2])
+        percentage_entry.insert(0,details[3])
+    else:
+        messagebox.showerror("no index","select a name")
+
+def delete():
+    index = display.curselection()
+    if index:
+        display.delete(index)
+        print(display.get(index))
+        print(marks)
+        del marks[display.get(index)]
+        clear_boxes()
+    else:
+        messagebox.showerror("no index","select a name")
+
+def open():
+    global marks
+    clear_list()
+    input_file = askopenfile()
+    if input_file:
+        marks = eval(input_file.read())
+        for key in marks:
+            display.insert(END,key)
+        title_label.config(text=os.path.basename(input_file.name))
+    else:
+        messagebox.showerror("file not selected","file not selected")
+
+def save():
+    user_file = asksaveasfile(defaultextension="*.txt")
+    if user_file:
+        print(marks,file=user_file)
+        clear_boxes()
+    else:
+        messagebox.showerror("save failed","file not saved")
 
 marks = {}
 
@@ -67,15 +119,15 @@ display = Listbox(root)
 display.grid(row=4,column=0,columnspan=5,padx=10,pady=10)
 display.bind("<<ListboxSelect>>",display_marks)
 
-edit_btn = Button(root,text="edit")
+edit_btn = Button(root,text="edit",command=edit)
 edit_btn.grid(row=5,column=0,padx=10,pady=10)
-del_btn = Button(root,text="delete")
+del_btn = Button(root,text="delete",command=delete)
 del_btn.grid(row=5,column=1,padx=10,pady=10)
-open_btn = Button(root,text="open")
+open_btn = Button(root,text="open",command=open)
 open_btn.grid(row=5,column=2,padx=10,pady=10)
 updateAdd_btn = Button(root,text="Update / Add",command=update_add)
 updateAdd_btn.grid(row=5,column=3,padx=10,pady=10)
-save_btn = Button(root,text="save")
+save_btn = Button(root,text="save",command=save)
 save_btn.grid(row=5,column=4,padx=10,pady=10)
 
 root.mainloop()
